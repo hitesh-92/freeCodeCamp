c36643dab10f5e9eb15c7c1a8abbbe46295ddc1a
hitesh-92 <hiteshgohil@hotmail.co.uk>
2018-03-23 06:22:44 +0000

    quote-machine

--- projects/quote-machine/script.js
+++ projects/quote-machine/script.js
@@ -1,18 +1,33 @@
-$(document).ready(function() {
+var q = []; var a = [];
+document.getElementById('button').addEventListener('click', loadQuote);
+function loadQuote(){
+  var xhr = new XMLHttpRequest(); q.shift();a.shift();
+  xhr.open('GET', 'quotes.json', true);
+  xhr.onload = function(){
+    if(this.status == 200){
 
-    $("#btn").on("click", function(){
-      // $(".message").html("Here is the message");
-      $.getJSON("./quotes.json", function(json){
+      var quotes = JSON.parse(this.responseText);
+      var x = Math.floor(Math.random() * 5);
+      // console.log(`\nx: ${x}`);// console.log(quotes[x].auth);
 
-        var html = "";
-        json.forEach(function(val) {
-          var keys = Object.keys(val);
-          html += "<div class='data'>";
-          keys.forEach(function(key){
-            html += "<strong>" + key + "</strong>: " + val[key] + "<br>";
-          });
-          html += "</div><br>"
-        });
-      })
-    });
-});
+      var quote = ''; var auth = '';
+      quote += `<h4>${quotes[x]['body']}</h4>`;
+      q.push(quotes[x]['body']);
+      auth += `<p>${quotes[x]['auth']}</p>`;
+      a.push(quotes[x]['auth']);
+
+      document.getElementById('quote').innerHTML = quote;
+      document.getElementById('auth').innerHTML = auth;
+    }
+
+  }
+  xhr.send();
+  console.log(`\nq: ${q}\na: ${a}`);
+}
+
+document.getElementById('tweet').addEventListener('click', tweet);
+function tweet(){
+  var post = `${q} - ${a}`;
+  console.log(`\n\n\nPOST: ${post}`);;
+   window.open("https://twitter.com/intent/tweet?text=" + post)
+}
