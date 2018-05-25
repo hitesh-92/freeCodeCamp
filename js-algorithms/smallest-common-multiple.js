f21b5f6ecc539cf113dd47a3ed462ba4883b8068
hitesh-92 <hiteshgohil@hotmail.co.uk>
2018-05-25 18:23:25 +0100

    update sum-all-primes

---
+++ js-algorithms/smallest-common-multiple.js
@@ -0,0 +1,17 @@
+function smallestCommons(arr) {
+  // while(true){
+  //
+  //   console.log(argu);
+  //
+  // }//while
+
+  //extract values
+  n1 = arguments[0][0]
+  n2 = arguments[0][1]
+
+
+
+}
+
+
+smallestCommons([1,5]);
--- js-algorithms/sum-all-primes.js
+++ js-algorithms/sum-all-primes.js
@@ -17,6 +17,7 @@ function sumPrimes(num) {
 
     return true;
   };//func
+
   //## this function finds primes numbers too
   // function isPrime(number) {
   //   var start = 2;
@@ -47,4 +48,36 @@ function sumPrimes(num) {
 }
 
 // sumPrimes(10);
-sumPrimes(977)
+// sumPrimes(977)
+
+
+//ANSWER
+function sumPrimes(num) {
+  //var to hold total value to return
+  let primes = new Array();
+  let total = 0;
+
+  function isPrime(value) {
+    if(value < 2){
+      return false;
+    }
+
+    for (var i = 2; i < value; i++) {
+        if(value%i==0)
+            return false;
+    }
+
+    return true;
+  };
+
+  for(let j = 2; j <= num; j++){
+    if(isPrime(j)) {primes.push(j)};
+  }
+
+  for (let k = 0; k < primes.length; k++) {
+    // console.log(primes[k]);
+    total += primes[k];
+  }
+
+  return total;
+}
